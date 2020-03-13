5d556379e3c44b3e9169b1e9b906a8431099e883
hyj2u <hyj2u94@gmail.com>
2020-03-13 17:26:34 +0900

    데이터 등록 및 테스트

---
+++ src/main/java/com/web/toyproject/springboot/web/PostsApiController.java
@@ -0,0 +1,20 @@
+package com.web.toyproject.springboot.web;
+
+import com.web.toyproject.springboot.service.posts.PostsService;
+import com.web.toyproject.springboot.web.dto.PostsSaveRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+public class PostsApiController {
+
+    private final PostsService postsService;
+
+    @PostMapping("/api/v1/posts")
+    public Long save(@RequestBody PostsSaveRequestDto requestDto){
+        return postsService.save(requestDto);
+    }
+}
