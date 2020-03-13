f984529213fcd2b0d7bf79bca59d9e9e1b5ad876
hyj2u <hyj2u94@gmail.com>
2020-03-13 16:46:48 +0900

    JPA 테스트코드

---
+++ src/test/java/com/web/toyproject/springboot/domain/posts/PostsRepositoryTest.java
@@ -0,0 +1,35 @@
+package com.web.toyproject.springboot.domain.posts;
+
+import org.assertj.core.api.Assertions;
+import org.junit.After;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+
+import static org.junit.Assert.*;
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PostsRepositoryTest {
+    @Autowired
+    PostsRepository postsRepository;
+
+    @After
+    public void cleanup(){
+        postsRepository.deleteAll();
+    }
+    @Test
+    public void select(){
+        String title="제목";
+        String content ="본문";
+        postsRepository.save(Posts.builder().title(title).content(content).author("hyj2u@neowiz.com").build());
+
+        List<Posts> postsList = postsRepository.findAll();
+        Posts posts = postsList.get(0);
+        Assertions.assertThat(posts.getTitle()).isEqualTo(title);
+        Assertions.assertThat(posts.getContent()).isEqualTo(content);
+    }
+}
