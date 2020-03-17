5a5913a52d1f04bce05566368fae0a481fbb36be
hyj2u <43366665+hyj2u@users.noreply.github.com>
2020-03-17 16:13:18 +0900

    데이터 수정, 삭제 기능

--- src/main/java/com/web/toyproject/springboot/web/IndexController.java
+++ src/main/java/com/web/toyproject/springboot/web/IndexController.java
@@ -1,10 +1,12 @@
 package com.web.toyproject.springboot.web;
 
 import com.web.toyproject.springboot.service.posts.PostsService;
+import com.web.toyproject.springboot.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 @RequiredArgsConstructor
@@ -18,5 +20,11 @@ public class IndexController {
     @GetMapping("/posts/save")
     public String postSave(){
         return "posts-save";
+    }
+    @GetMapping("/posts/update/{id}")
+    public String postsUpdate(@PathVariable Long id, Model model){
+        PostsResponseDto dto = postsService.findById(id);
+        model.addAttribute("post", dto);
+        return "posts-update";
     }
 }
