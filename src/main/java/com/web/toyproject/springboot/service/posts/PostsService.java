96e1f7983da1f1e4ee661b6d18411d69863366a4
hyj2u <43366665+hyj2u@users.noreply.github.com>
2020-03-17 15:34:43 +0900

    전체 조회 기능

--- src/main/java/com/web/toyproject/springboot/service/posts/PostsService.java
+++ src/main/java/com/web/toyproject/springboot/service/posts/PostsService.java
@@ -2,13 +2,16 @@ package com.web.toyproject.springboot.service.posts;
 
 import com.web.toyproject.springboot.domain.posts.Posts;
 import com.web.toyproject.springboot.domain.posts.PostsRepository;
+import com.web.toyproject.springboot.web.dto.PostsListResponseDto;
 import com.web.toyproject.springboot.web.dto.PostsResponseDto;
 import com.web.toyproject.springboot.web.dto.PostsSaveRequestDto;
 import com.web.toyproject.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
-import javax.transaction.Transactional;
+import org.springframework.transaction.annotation.Transactional;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -29,4 +32,9 @@ public class PostsService {
         Posts entity = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id="+id));
         return  new PostsResponseDto(entity);
     }
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc(){
+        return postsRepository.findAllDesc().stream().map(PostsListResponseDto::new).collect(Collectors.toList());
+    }
+
 }
---
+++ src/main/java/com/web/toyproject/springboot/web/dto/PostsListResponseDto.java
@@ -0,0 +1,21 @@
+package com.web.toyproject.springboot.web.dto;
+
+import com.web.toyproject.springboot.domain.posts.Posts;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Getter
+public class PostsListResponseDto {
+    private Long id;
+    private String title;
+    private String author;
+    private LocalDateTime modifiedTime;
+
+    public PostsListResponseDto(Posts entity){
+        this.id = entity.getId();
+        this.title = entity.getTitle();
+        this.author = entity.getAuthor();
+        this.modifiedTime = entity.getModifiedDate();
+    }
+}
