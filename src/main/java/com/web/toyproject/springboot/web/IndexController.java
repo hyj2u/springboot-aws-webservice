95a1baf2efa3d8bfbca027f905db9a6de4a695db
hyj2u <43366665+hyj2u@users.noreply.github.com>
2020-03-18 10:00:26 +0900

    @Login User 생성

--- src/main/java/com/web/toyproject/springboot/web/IndexController.java
+++ src/main/java/com/web/toyproject/springboot/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.web.toyproject.springboot.web;
 
+import com.web.toyproject.springboot.config.auth.LoginUser;
 import com.web.toyproject.springboot.config.auth.dto.SessionUser;
 import com.web.toyproject.springboot.service.posts.PostsService;
 import com.web.toyproject.springboot.web.dto.PostsResponseDto;
@@ -15,11 +16,10 @@ import javax.servlet.http.HttpSession;
 @RequiredArgsConstructor
 public class IndexController {
     private final PostsService postsService;
-    private final HttpSession httpSession;
+
     @GetMapping("/")
-    public String index(Model model){
+    public String index(Model model, @LoginUser SessionUser user){
         model.addAttribute("posts", postsService.findAllDesc());
-        SessionUser user = (SessionUser)httpSession.getAttribute("user");
         if(user != null){
             model.addAttribute("userName", user.getName());
         }
