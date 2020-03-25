bcb5843387a4cf6999d517ad3a6775b3ba9eadc6
hyj2u <43366665+hyj2u@users.noreply.github.com>
2020-03-25 13:24:33 +0900

    db 연동 및 보안 설정

--- src/main/java/com/web/toyproject/springboot/domain/posts/PostsRepository.java
+++ src/main/java/com/web/toyproject/springboot/domain/posts/PostsRepository.java
@@ -2,9 +2,10 @@ package com.web.toyproject.springboot.domain.posts;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
-
+@Repository
 public interface PostsRepository extends JpaRepository<Posts, Long> {
 
     @Query("SELECT p FROM Posts p ORDER BY p.id DESC")
